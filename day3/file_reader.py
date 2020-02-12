010632e68694d9e7e0e60f8c7b8aaa8489f557c7
Kevin Z <kevinz@KevindeMBP.lan>
2020-02-12 16:29:02 +0800

    don't know shit

--- day3/file_reader.py
+++ day3/file_reader.py
@@ -29,6 +29,19 @@ file_path = GetPath()
 # file_path = '/Users/kevinz/workspace/python/practise/day3/text_files/pai.txt'
 
 with open(file_path) as file_object:
-    contents = file_object.read()
-    print(contents)
+    # contents = file_object.read()
+    # print(contents)
+    lines = file_object.readlines()
 
+pi_string = ' '
+for line in lines:
+    pi_string += line.strip()
+
+# print(pi_string[:52] + "...")
+# print(len(pi_string))
+
+birthday = input("Enter your birthday, in the form mmddyy: ")
+if birthday in pi_string:
+    print("Your birthday appears in the first million digits of pi!")
+else:
+    print("Your birthday does not appear in the first million digits of pi.")
---
+++ day3/wirte_message.py
@@ -0,0 +1,6 @@
+filename = './text_files/programming.txt'
+
+with open(filename, 'a') as file_object:
+    file_object.write("I love coding.\n")
+    file_object.write("I love creating new games.\n")
+
